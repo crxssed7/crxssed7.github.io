63f861826f56ec77b1512188f6a194fc1bc199ec
Tanveer Najib <tanveernajib@gmail.com>
2022-09-07 16:49:02 +0100

    updates projects page

---
+++ assets/js/fullPage.js
@@ -0,0 +1,8 @@
+// Code By Webdevtrick ( https://webdevtrick.com )
+$('#fullpage').fullpage({
+    sectionSelector: '.vertical-scrolling',
+    navigation: true,
+    slidesNavigation: true,
+    controlArrows: false,
+    anchors: ['tankobon', 'kaboom', 'passguard', 'comicpeeps'],
+});
