bffa564658eba2aa1f43a1aaa2093d9f4b757934
Tanveer Najib <48864420+crxssed7@users.noreply.github.com>
2022-10-01 19:08:02 +0100

    fix month bug

--- manga/assets/js/index.js
+++ manga/assets/js/index.js
@@ -222,6 +222,6 @@ function handleError(error) {
 }
 
 function generateDate(year, month, day) {
-    var date = new Date(year, month, day);
+    var date = new Date(year, month - 1, day);
     return date.toLocaleDateString();
 }
