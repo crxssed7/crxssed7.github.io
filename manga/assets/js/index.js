697b01d8ab716c7f52c7c5eb262f3595dc0d6992
Tanveer Najib <tanveernajib@gmail.com>
2023-01-07 15:59:49 +0000

    javascript. the bane of my existence.

--- manga/assets/js/index.js
+++ manga/assets/js/index.js
@@ -1,6 +1,49 @@
+var listData = `
+lists {
+    name
+    isCustomList
+    isSplitCompletedList
+    status
+    name
+    entries {
+      id
+      progress
+      updatedAt
+      completedAt {
+        year
+        month
+        day
+      }
+      startedAt {
+        year
+        month
+        day
+      }
+      notes
+      media {
+        id
+        title {
+          romaji
+          english
+          native
+          userPreferred
+        }
+        coverImage {
+          extraLarge
+          large
+          medium
+          color
+        }
+        bannerImage
+        chapters
+      }
+    }
+  }
+`
+
 var query = `
 {
-    MediaListCollection(userName: "crxssed", type: MANGA, sort: UPDATED_TIME_DESC) {
+    main: MediaListCollection(userName: "crxssed", type: MANGA, status_not: COMPLETED, sort: UPDATED_TIME_DESC) {
       user {
         id
         name
@@ -9,48 +52,155 @@ var query = `
           medium
         }
       }
-      lists {
-        name
-        isCustomList
-        isSplitCompletedList
-        status
-        name
-        entries {
-          id
-          progress
-          completedAt {
-            year
-            month
-            day
-          }
-          startedAt {
-            year
-            month
-            day
-          }
-          notes
-          media {
-            id
-            title {
-              romaji
-              english
-              native
-              userPreferred
-            }
-            coverImage {
-              extraLarge
-              large
-              medium
-              color
-            }
-            bannerImage
-            chapters
-          }
-        }
-      }
+      ${listData}
+    }
+    completed: MediaListCollection(userName: "crxssed", type: MANGA, status: COMPLETED, sort: FINISHED_ON_DESC) {
+      ${listData}
     }
   }
+  
 `;
+
+class Manga {
+    progressBarStatuses = ["READING"];
+    dateStatuses = ["READING", "COMPLETED"];
+
+    constructor(data, status) {
+        this.data = data
+        this.status = status
+        this.setName()
+        this.setProgressAndMaximum()
+        this.setColor()
+        this.setCoverImage()
+    }
+
+    generateNotesHtml() {
+        var notes = this.data['notes'];
+        var notesStr = '';
+        if (notes === null) {
+            notesStr = '';
+        } else {
+            notesStr = `data-bs-toggle="tooltip" data-bs-title='${this.stripSafe(notes)}'`;
+        }
+        return notesStr
+    }
+
+    generateDateHtml() {
+        var output = "";
+        switch (this.status) {
+            case "READING":
+                var startedAt = this.data['startedAt'];
+                var startedAtString = "";
+                if (startedAt['year']) {
+                    var d = this.generateDate(Number(startedAt['year']), Number(startedAt['month']), Number(startedAt['day']));
+                    startedAtString = `Started: ${d}`
+                }
+                output = startedAtString
+            break;
+            
+            case "COMPLETED":
+                var completedAt = this.data['completedAt'];
+                var completedAtString = "";
+                if (completedAt['year']) {
+                    var d = this.generateDate(Number(completedAt['year']), Number(completedAt['month']), Number(completedAt['day']));
+                    completedAtString = `Completed: ${d}`
+                }
+                output = completedAtString;
+            break;
+        }
+        return `<p style="margin-bottom: 0px;"><small>${output}</small></p>`
+    }
+
+    setProgressAndMaximum() {
+        this.progress = Number(this.data['progress']);
+        var chapters = this.data['media']['chapters'];
+        this.max = Number(chapters);
+    }
+
+    setColor() {
+        var color = this.data['media']['coverImage']['color'];
+        if (color === null || color === undefined) {
+            color = '#a0a0a0';
+        }
+        this.color = color;
+    }
+
+    setName() {
+        var mangaName = this.data['media']['title']['english']
+        if (mangaName === null || mangaName === undefined) {
+            mangaName = this.data['media']['title']['romaji'];
+        }
+        this.name = mangaName;
+    }
+
+    setCoverImage() {
+        this.coverImage = this.data['media']['coverImage']['medium']
+    }
+
+    generateDate(year, month, day) {
+        var date = new Date(year, month - 1, day);
+        return date.toLocaleDateString();
+    }
+    
+    stripSafe(value) {
+        return value.replaceAll('<', '').replaceAll('>', '').replaceAll("'", '&#39;').replaceAll('"', '&#34;');
+    }
+
+    buildProgessBar() {
+        var val = this.progress;
+        var max = this.max;
+        if (max === 0) {
+            max = 1;
+            val = 1
+        }
+
+        var percentage = (val / max) * 100;
+
+        return `
+    <div class="progress" style="height: 5px;">
+        <div class="progress-bar" role="progressbar" style="width: ${percentage}%; background-color: ${this.color};" aria-valuenow="${this.progress}" aria-valuemin="0" aria-valuemax="${this.max}"></div>
+    </div>
+        `;
+    }
+    
+    toHtml() {
+        // Build Progress Bar
+        var progressBarHtml = "";
+        if (this.progressBarStatuses.includes(this.status.toUpperCase())) {
+            progressBarHtml = this.buildProgessBar();
+        }
+    
+        // Build Date String
+        var dateHtml = ""
+        if (this.dateStatuses.includes(this.status.toUpperCase())) {
+            dateHtml = this.generateDateHtml()
+        }
+    
+        // Build Fraction String
+        var fractionHtml = `<p style="margin-bottom: 0px;"><small>${this.progress} / ${this.max}</small></p>`
+        if (this.max === 0) {
+            fractionHtml = `<p style="margin-bottom: 0px;"><small>${this.progress} / ?</small></p>`
+        }
+        
+        return `
+        <div class="entry" style="border-color: ${this.color};" ${this.generateNotesHtml()}>
+            <div class="flex">
+                <div class="entry-img" style="background-image: url('${this.coverImage}');">
+                </div>
+                <div class="entry-content">
+                    <div class="entry-name">
+                        <h5>${this.name}</h5>
+                    </div>
+                    <div>
+                        ${dateHtml}
+                        ${fractionHtml}
+                        ${progressBarHtml}
+                    </div>
+                </div>
+            </div>
+        </div>`;
+    }
+}
 
 // Define the config we'll need for our Api request
 var url = 'https://graphql.anilist.co',
@@ -77,15 +227,27 @@ function handleResponse(response) {
 }
 
 function handleData(data) {
-    var completed = data["data"]["MediaListCollection"]["lists"].find(obj => {
+    var completed = data["data"]["completed"]["lists"].find(obj => {
         return obj.name === "Completed"
     })["entries"];
-    var reading = data["data"]["MediaListCollection"]["lists"].find(obj => {
+
+    var reading = data["data"]["main"]["lists"].find(obj => {
         return obj.name === "Reading"
     })["entries"];
-    var planning = data["data"]["MediaListCollection"]["lists"].find(obj => {
+    
+    var planning = data["data"]["main"]["lists"].find(obj => {
         return obj.name === "Planning"
     })["entries"];
+
+    var collecting = data["data"]["main"]["lists"].find(obj => {
+        return obj.name === "Collecting"
+    })["entries"];
+    var collecting_completed = data["data"]["completed"]["lists"].find(obj => {
+        return obj.name === "Collecting"
+    })["entries"];
+    collecting = collecting.concat(collecting_completed);
+
+    console.log(collecting)
 
     var read = 0;
     var left = 0;
@@ -93,6 +255,7 @@ function handleData(data) {
     var readingEl = document.getElementById('reading');
     var completedEl = document.getElementById('completed');
     var plannedEl = document.getElementById('planned');
+    var collectingEl = document.getElementById('collecting');
     var chaptersReadEl = document.getElementById('chapters_read');
     var chaptersLeftEl = document.getElementById('chapters_left');
     var totalEl = document.getElementById('total');
@@ -100,172 +263,57 @@ function handleData(data) {
     var readingCountEl = document.getElementById('reading_count');
     var completedCountEl = document.getElementById('completed_count');
     var plannedCountEl = document.getElementById('planned_count');
+    var collectingCountEl = document.getElementById('collecting_count');
 
-    avatarEl.setAttribute('src', data["data"]["MediaListCollection"]['user']['avatar']['medium']);
+    avatarEl.setAttribute('src', data["data"]["main"]['user']['avatar']['medium']);
 
     readingCountEl.innerText = reading.length;
     completedCountEl.innerText = completed.length;
     plannedCountEl.innerText = planning.length;
+    collectingCountEl.innerText = collecting.length;
     totalEl.innerText = reading.length + completed.length + planning.length;
 
     reading.map((e) => {
-        var notes = e['notes'];
-        var notesStr = '';
-        if (notes == null) {
-            notesStr = '';
-        } else {
-            notesStr = `data-bs-toggle="tooltip" data-bs-title='${stripSafe(notes)}'`;
+        var manga = new Manga(e, "READING")
+
+        if (manga.max > 0) {
+            left += manga.max - manga.progress;
         }
+        read += manga.progress;
 
-        var startedAt = e['startedAt'];
-        var startedAtString = '';
-        if (startedAt['year']) {
-            var d = generateDate(Number(startedAt['year']), Number(startedAt['month']), Number(startedAt['day']));
-            startedAtString = `Started: ${d}`
-        }
-
-        var progress = e['progress'];
-        var chapters = e['media']['chapters'];
-        var max = 0;
-        if (chapters == null || chapters == undefined) {
-            chapters = '?';
-            max = progress;
-        } else {
-            max = Number(chapters);
-            left += max - progress;
-        }
-
-        var percentage = (progress / max) * 100;
-
-        var color = e['media']['coverImage']['color'];
-        if (color == null || color == undefined) {
-            color = '#a0a0a0';
-        }
-
-        var chapterName = e['media']['title']['english']
-        if (chapterName == null || chapterName == undefined) {
-            chapterName = e['media']['title']['romaji'];
-        }
-
-        read += progress;
-
-        var html = `
-        <div class="entry" style="border-color: ${color};" ${notesStr}>
-            <div class="flex">
-                <div class="entry-img" style="background-image: url('${e['media']['coverImage']['medium']}');">
-                </div>
-                <div class="entry-content">
-                    <div class="entry-name">
-                        <h5>${chapterName}</h5>
-                    </div>
-                    <div>
-                        <p style="margin-bottom: 0px;"><small>${startedAtString}</small></p>
-                        <p style="margin-bottom: 0px;"><small>${progress} / ${chapters}</small></p>
-                        <div class="progress" style="height: 5px;">
-                            <div class="progress-bar" role="progressbar" style="width: ${percentage}%; background-color: ${color};" aria-valuenow="${progress}" aria-valuemin="0" aria-valuemax="${max}"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>`;
+        var html = manga.toHtml();
 
         readingEl.innerHTML += html;
     })
 
     completed.map((e) => {
-        var notes = e['notes'];
-        var notesStr = '';
-        if (notes == null) {
-            notesStr = '';
-        } else {
-            notesStr = `data-bs-toggle="tooltip" data-bs-title='${stripSafe(notes)}'`;
-        }
+        var manga = new Manga(e, "COMPLETED");
 
-        var completedAt = e['completedAt'];
-        var completedAtString = '';
-        if (completedAt['year']) {
-            var d = generateDate(Number(completedAt['year']), Number(completedAt['month']), Number(completedAt['day']));
-            completedAtString = `Completed: ${d}`
-        }
+        read += manga.progress;
 
-        var chapters = e['media']['chapters'];
-        if (chapters == null || chapters == undefined) {
-            chapters = '?';
-        }
-
-        var color = e['media']['coverImage']['color'];
-        if (color == null || color == undefined) {
-            color = '#a0a0a0';
-        }
-
-        var chapterName = e['media']['title']['english']
-        if (chapterName == null || chapterName == undefined) {
-            chapterName = e['media']['title']['romaji'];
-        }
-
-        read += e['progress'];
-
-        var html = `
-        <div class="entry" style="border-color: ${color};"  ${notesStr}>
-            <div class="flex">
-                <div class="entry-img"
-                    style="background-image: url('${e['media']['coverImage']['medium']}');">
-                </div>
-                <div class="entry-content">
-                    <div class="entry-name">
-                        <h5>${chapterName}</h5>
-                    </div>
-                    <p style="margin-bottom: 0px;"><small>${completedAtString}</small></p>
-                    <p style="margin-bottom: 0px;"><small>${e['progress']} / ${chapters}</small></p>
-                </div>
-            </div>
-        </div>`;
+        var html = manga.toHtml();
 
         completedEl.innerHTML += html;
     })
 
     planning.map((e) => {
-        var notes = e['notes'];
-        var notesStr = '';
-        if (notes == null) {
-            notesStr = '';
-        } else {
-            notesStr = `data-bs-toggle="tooltip" data-bs-title='${stripSafe(notes)}'`;
-        }
+        var manga = new Manga(e, "PLANNED");
 
-        var chapters = e['media']['chapters'];
-        if (chapters == null || chapters == undefined) {
-            chapters = '?';
-        } else {
-            left += chapters;
-        }
+        left += manga.max;
 
-        var color = e['media']['coverImage']['color'];
-        if (color == null || color == undefined) {
-            color = '#a0a0a0';
-        }
-
-        var chapterName = e['media']['title']['english']
-        if (chapterName == null || chapterName == undefined) {
-            chapterName = e['media']['title']['romaji'];
-        }
-
-        var html = `
-        <div class="entry" style="border-color: ${color};"  ${notesStr}>
-            <div class="flex">
-                <div class="entry-img"
-                    style="background-image: url('${e['media']['coverImage']['medium']}');">
-                </div>
-                <div class="entry-content">
-                    <div class="entry-name">
-                        <h5>${chapterName}</h5>
-                    </div>
-                    <p style="margin-bottom: 0px;"><small>${e['progress']} / ${chapters}</small></p>
-                </div>
-            </div>
-        </div>`;
+        var html = manga.toHtml();
 
         plannedEl.innerHTML += html;
+    })
+
+    collecting.map((e) => {
+        var manga = new Manga(e, "COLLECTING");
+
+        left += manga.max;
+
+        var html = manga.toHtml();
+
+        collectingEl.innerHTML += html;
     })
 
     chaptersReadEl.innerText = read;
@@ -278,13 +326,4 @@ function handleData(data) {
 function handleError(error) {
     // alert('Error, check console');
     console.error(error);
-}
-
-function generateDate(year, month, day) {
-    var date = new Date(year, month - 1, day);
-    return date.toLocaleDateString();
-}
-
-function stripSafe(value) {
-    return value.replaceAll('<', '').replaceAll('>', '').replaceAll("'", '&#39;').replaceAll('"', '&#34;');
 }
